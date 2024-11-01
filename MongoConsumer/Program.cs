6546fd2694758f144cb1ededef71b397b9b9b705
Tomer <tomer.cohen243@gmail.com>
2024-11-01 18:06:36 +0200

    [feat]initial creation of console app

---
+++ MongoConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
